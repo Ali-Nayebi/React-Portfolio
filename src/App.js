d5434897f77923ac5da23797342658f5d7bf6df1
Ali Nayebi <elderekkoo@gmail.com>
2023-07-16 12:13:31 +0330

    navbar set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react';
+import useMediaQuery from './hooks/useMediaQuery';
+import Navbar from './scenes/Navbar';
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreen = useMediaQuery('(min-width: 1060px)');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage('home');
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      first
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 }
