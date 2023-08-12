13a2a21dfa7b39a30610ea2c13139672504b8704
Ali Nayebi <elderekkoo@gmail.com>
2023-08-12 19:32:34 +0330

    contact section complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MySkills from './scenes/MySkills';
 import Projects from './scenes/Projects';
 import Testimonials from './scenes/Testimonials';
 import Contact from './scenes/Contact';
+import Footer from './scenes/Footer';
 import DotGroup from './scenes/DotGroup';
 import Landing from './scenes/Landing';
 import LineGradient from './components/LineGradient';
@@ -58,6 +59,7 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
         <Contact />
       </div>
+      <Footer />
     </div>
   );
 }
